2c29c84b070507f58a994019040f420752a5ebfc
Tajudeen khabab Akanbi <152615493+TajjDev@users.noreply.github.com>
2024-11-16 16:09:05 +0100

    Add files via upload
    hh

--- src/assets/component/get started/GetStarted.jsx
+++ src/assets/component/get started/GetStarted.jsx
@@ -8,7 +8,7 @@ const GetStarted = () => {
   return (
     <div id='offer'>
         <h2>what we <span>offer</span></h2>
-        <p id='offerp'>By using the <span>Vote</span>City platform, you gain access to a range of ability to get the involved with the community</p>
+        <p id='offerp'>By using the <span>Vote</span>City platform, you gain access to a wider range of ability by getting more involved with the community</p>
         <div id="getSt">
             <Get start={"/#"} h4Txt={"Poll Voting"} pTxt={"Create your own polls, cast your vote, join the conversation and make an impact."} img={img1}/>
             <Get start={"/#"} h4Txt={"Event Booking"} pTxt={"Announce your next big moment, secure your spot and book event with just a few clicks."} img={img2}/>
